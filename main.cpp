09a424f7ea36fb890cab74853b0c3bff51690f59
ERJennings <erjennings13@gmail.com>
2020-11-19 19:40:39 -0800

    Did answers.txt, checking for errors but everything seems correct

--- main.cpp
+++ main.cpp
@@ -9,7 +9,8 @@
 #include "print_ts.h"
 
 int main() {
-	startThreads("word",6,P5,8,777);
+	//word,number of threads,number per line,number to print per thread,millisecond delay
+	startThreads("word",3,P4,2,777);
 	joinThreads();
 }
 
